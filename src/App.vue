76096cd60a7de094a8a4cd84fa204addabf885c6
Pranjalya <pranjalyawarrior@gmail.com>
2019-11-10 00:02:43 +0530

    Some changes in child1 branch

--- src/App.vue
+++ src/App.vue
@@ -7,28 +7,32 @@
       <span class="glyphicon glyphicon-facetime-video" aria-hidden="true"></span>Call
     </button>
     <button v-on:click="showMyFace">Display</button>
+    <br />
+    <button v-on:click="disconnectCall">Disconnect</button>
   </div>
 </template>
 
 <script>
 var PouchDB = require("pouchdb").default;
 
-document.addEventListener('deviceready', function() {
-  console.log('cordova.plugins.CordovaCall is now available');
+document.addEventListener("deviceready", function() {
+  console.log("cordova.plugins.CordovaCall is now available");
   var cordovaCall = cordova.plugins.CordovaCall; //not necessary, but might be more convenient
 });
 
 export default {
   data() {
     return {
-    localDB: {},
-    remoteDB: {},
-    pc: {},
-    database: {},
-    yourVideo: {},
-    friendsVideo: {},
-    yourId: 0,
-    servers: {}
+      localDB: {},
+      remoteDB: {},
+      pc: {},
+      database: {},
+      yourVideo: {},
+      friendsVideo: {},
+      yourId: "",
+      servers: {},
+      receiversId: "",
+      updated: false
     };
   },
 
@@ -41,7 +45,8 @@ export default {
     this.startRep();
     this.yourVideo = document.getElementById("yourVideo");
     this.friendsVideo = document.getElementById("friendsVideo");
-    this.yourId = Math.floor(Math.random() * 1000000000);
+    this.yourId = "Kvothe";
+    this.receiversId = "Denna";
     console.log("My ID : ", this.yourId);
     //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
     this.servers = {
@@ -52,14 +57,17 @@ export default {
           urls: "turn:numb.viagenie.ca",
           credential: "kingkiller",
           username: "pranjalyawarrior@gmail.com"
-        },
+        }
       ]
     };
 
     this.pc = new RTCPeerConnection(this.servers);
     this.pc.onicecandidate = event =>
       event.candidate
-        ? this.sendMessage(this.yourId, JSON.stringify({ ice: event.candidate }))
+        ? this.sendMessage(
+            this.yourId,
+            JSON.stringify({ ice: event.candidate })
+          )
         : console.log("Sent All Ice");
     this.pc.onaddstream = event => (friendsVideo.srcObject = event.stream);
     //  this.database.on('child_added', this.readMessage);
@@ -69,37 +77,60 @@ export default {
   methods: {
     showMyFace() {
       navigator.mediaDevices
-        .getUserMedia({ audio: true, video: true })
+        .getUserMedia({ audio: false, video: true })
         .then(stream => (this.yourVideo.srcObject = stream))
-        .then(stream => this.pc.addStream(stream)).catch(console.log);
+        .then(stream => this.pc.addStream(stream))
+        .catch(console.log);
     },
 
     showFriendsFace() {
-      this.pc.createOffer()
+      this.pc
+        .createOffer()
         .then(offer => this.pc.setLocalDescription(offer))
         .then(() =>
-          this.sendMessage(this.yourId, JSON.stringify({ sdp: this.pc.localDescription }))
+          this.sendMessage(
+            this.yourId,
+            JSON.stringify({ sdp: this.pc.localDescription })
+          )
         );
     },
 
     sendMessage(senderId, data) {
-      console.log("Sending message", this.yourId.toString());
-      this.localDB.get(this.yourId.toString())
+      console.log("Sending message", this.yourId);
+      this.localDB
+        .get(this.yourId)
         .then(doc => {
-          delete doc._rev;
-          doc.sender = senderId;
-          doc.message = data;
-          this.localDB.put(doc).then(()=>{ console.log("inserted after getting")}).catch(err => {});
+          if(!this.updated){
+            this.updated = true;
+           this.localDB.put({
+            _id: this.yourId,
+            sender: senderId,
+            message: data,
+            force: true
+          });
+          }
         })
         .catch(err => {
           if (err.status == 404) {
-            this.localDB
-              .put({
-                _id: this.yourId.toString(),
-                sender: senderId,
-                message: data
-              }).then(()=>{ console.log("inserted")}).catch(err => {});
+            console.log("inside 404");
+            return this.localDB.put({
+              _id: this.yourId,
+              sender: senderId,
+              message: data,
+            });
+          } else if (err.status == 409 ) {
+            return this.localDB.put({
+              _id: this.yourId,
+              sender: senderId,
+              message: data,
+              force: true
+            });
+          } else {
+            console.log(err);
           }
+        })
+        .catch(err => {
+          console.log(err);
         });
       //  var msg = this.database.push({ sender: senderId, message: data });
       //  msg.remove();
@@ -128,8 +159,8 @@ export default {
             )
             .catch(err => {
               console.log(err);
-            }); }
-        else if (msg.sdp.type == "answer"){
+            });
+        } else if (msg.sdp.type == "answer") {
           this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
           console.log("Answer received");
         }
@@ -143,7 +174,7 @@ export default {
         retry: true
       })
         .on("change", function(info) {
-        //  console.log("Changes: made", info);
+          //  console.log("Changes: made", info);
         })
         .on("paused", function(err) {
           console.log("Replication: paused", err);
@@ -171,10 +202,28 @@ export default {
           include_docs: true
         })
         .on("change", change => {
-        //  console.log("Change - ", change);
-        if(change.id != this.yourId)
-          this.readMessage(change);
+          //  console.log("Change - ", change);
+          if (change.id == this.receiversId) 
+            this.readMessage(change);
+        })
+        .on("error", error => {
+          console.log("Change error", err);
         });
+    },
+
+    disconnectCall() {
+      this.pc.close();
+      this.localDB.get(this.yourId)
+        .then(doc => {
+          doc._deleted = true;
+          return this.localDB.put(doc);
+        })
+        .then(function(result) {})
+        .catch(function(err) {
+          console.log(err);
+        });
+      this.updated = false;
+      this.pc = new RTCPeerConnection(this.servers);
     }
   }
 };
